1a265fb696205a8674deef5ff020d7ec0ad5154e
MuriloMatos <115664045+MuriloOMatos@users.noreply.github.com>
2023-07-24 11:31:42 -0300

    Add files via upload

---
+++ aula1.py
@@ -0,0 +1,9 @@
+print('murilo')
+#comentario pode ser feito com o #
+
+'''
+docstring e um comando sendo assim e lido pelo python mas bastante utilizado como comentario e sendo multi linhas 
+assim 
+
+
+'''
